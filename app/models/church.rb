e12e3e4560d1067b85fd6b6b5a3b4fba7d784da8
Damilola Odelola <damiiodelola@gmail.com>
2014-07-02 16:11:42 +0100

    test passing for screen scraping list of churches

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,4 +1,25 @@
 class Church < ActiveRecord::Base
 
+	def self.churches_list
+		doc = Nokogiri::HTML(open("http://www.adventistchurches.org.uk/churches.php?Field=SEC%20(London)"))
+		# find out how to gsub on mapped objects
+		
+		doc.css("tr>td>p").each do |c|c.text
+			@name = c.children[0].text.chomp.strip.gsub(":", "")
+			if c.children[1] != nil
+				@address = c.children[1].text.chomp.strip.gsub(".  (See", "")
+			end
+			if c.children[5] != nil
+				@phone = c.children[5].text.chomp.strip.gsub(") Phone: ", "")
+			end
+			church = Church.new
+			church.name = @name
+			church.address = @address 
+			church.phone = @phone
+			church.save!
+		end
+
+		
+	end
 
 end
--- spec/models/church_spec.rb
+++ spec/models/church_spec.rb
@@ -1,2 +1,16 @@
 require 'spec_helper'
 
+describe Church do
+		context "Given a church exists" do
+			before do 
+				Church.churches_list
+			end
+
+			it "should get churches in database" do
+				expect(Church.first.name).to eq("Balham")
+				expect(Church.first.phone).to eq("020 8673 8038")
+				expect(Church.first.address).to eq("83 Elmfield Road  Balham London SW17 8AD")
+			end
+
+		end
+end
