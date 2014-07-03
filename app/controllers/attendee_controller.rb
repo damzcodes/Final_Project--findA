f360db23299cf4083d6a3dc1052ed941c07fc32d
Damilola Odelola <damiiodelola@gmail.com>
2014-07-03 10:51:26 +0100

    user can login through facebook

--- app/controllers/attendee_controller.rb
+++ app/controllers/attendee_controller.rb
@@ -1,2 +1,49 @@
 class AttendeeController < ApplicationController
+
+	def index
+		render :index
+	end
+
+		
+	def authorise_fb
+		redirect_to fb_oauth_client.auth_code.authorize_url(
+			:redirect_uri => fb_oauth_callback_url,
+			:scope => %w(email user_about_me user_friends).join(",")
+		)
+		# binding.pry
+	end
+
+	def fb_oauth_callback
+		access_token = fb_oauth_client.auth_code.get_token(
+			params[:action],
+			:redirect_uri => fb_oauth_callback_url,
+			:parse => :query
+		)
+		# binding.pry
+
+
+		 attendee_info = JSON(access_token.get('http://graph.facebook.com/endpoint?key=value&access_token=app_id|app_secret').body).symbolize_keys
+
+		@attendee = Attendee.find_or_create_by!(
+			:fb_id => attendee_info[:id],
+			:name => attendee_info[:name], 
+		)
+
+		@attendee.access_token = access_token.token
+		@attendee.save!
+
+		redirect_to attendees_path
+	end
+
+	private
+	def fb_oauth_client
+	  @fb_oauth_client ||= OAuth2::Client.new(
+	     FindA::Application.config.fb_id,
+	     FindA::Application.config.fb_secret,
+	     :site => "https://graph.facebook.com",
+	     :token_url => '/oauth/access_token'
+	  )
+	end	
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root to: 'attendee#index'
+  resources :attendee, :only => [:index, :create, :show, :delete]do
+    collection do
+      get :authorise_fb    
+    end
+  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'fb_oauth_callback', {
+    :to => 'attendee#fb_oauth_callback',
+    :as => :fb_oauth_callback
+  }
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :church, :only => [:index, :create, :show]
 end
