66e96289725f205db381fc50cac8b3a836eedcbd
Damilola Odelola <damiiodelola@gmail.com>
2014-07-09 20:57:05 +0100

    validation fro church_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ActiveRecord::Base
-	belongs_to :churches
-	has_many :attendees, :through => :event_attendees
+	belongs_to :church
+	has_many :attendee, :through => :event_attendees
 	has_many :event_attendees
 
 	geocoded_by :address, :latitude => :lat, :longitude => :lng
 	after_validation :geocode
+
+	validates :church_id, presence: true
 end
