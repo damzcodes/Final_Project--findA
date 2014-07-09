e419b077f1a29e970f07af970cede68272f8affb
Damilola Odelola <damiiodelola@gmail.com>
2014-07-09 10:14:52 +0100

    added new & create actions

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,17 +1,27 @@
 class EventController < ApplicationController
 
-# def index
-# end
+	def index
+	end
 
-# def create
-# 	@event = Event.new(allowed_params)
-# 	@event.save!
-# end
+	def create
+		@event = Event.new
+		if @event.save respond_to do |format|
+				format.html {redirect_to event_path(@event), :notice => "Your event has been saved"}
+				format.js
+			end
+		else
+			render :new
+		end
+	end
 
-# private
-# def(allowed_params)
-# 	params.require(:event).permit(:name, :address, :start_time, :end_time, :type, :description, :date)
-# end
+	def new
+		@event = Event.new
+	end
+
+
+	def edit
+		@event = Event.find_or_create_by!(name: Event.name)
+	end
 
 
 end
