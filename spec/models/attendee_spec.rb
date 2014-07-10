9861dc8e96b9f6b558968d75c31cacaa7f311c88
Damilola Odelola <damiiodelola@gmail.com>
2014-07-10 17:29:19 +0100

    syntax fixes

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Attendees do
+describe Attendee do
  	it {should have_many(:event_attendee)}
 	it {should have_many(:events).through(:event_attendee)}
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Events do
+describe Event do
 	it {should belongs_to(:churches)}
 	it {should have_many(:event_attendee)}
 	it {should have_many(:attendees).through(:event_attendee)}
