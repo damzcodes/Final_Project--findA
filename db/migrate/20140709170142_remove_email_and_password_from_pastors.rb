8f5b3ab85f93e708efa78d33a8c02f36270e0303
Damilola Odelola <damiiodelola@gmail.com>
2014-07-10 18:18:43 +0100

    removed migration

--- db/migrate/20140709170142_remove_email_and_password_from_pastors.rb
+++
@@ -1,6 +0,0 @@
-class RemoveEmailAndPasswordFromPastors < ActiveRecord::Migration
-  def change
-    remove_column :pastors, :email, :string
-    remove_column :pastors, :password, :string
-  end
-end
